f9958e0e6e1d9195b303083eb38462dea279993e
Luca Maroni <lcmaroni77@gmail.com>
2015-03-22 22:26:11 +0100

    delete code comments

--- admin/class-bl-slider-cms-manager-admin.php
+++ admin/class-bl-slider-cms-manager-admin.php
@@ -146,7 +146,6 @@ class Bl_Slider_Cms_Manager_Admin {
 
     function delete_cache_updating_post( $post_id, $post, $update ) {
 
-        // If this isn't a 'book' post, don't update it.
         if ( 'bl-slider' != $post->post_type ) {
             return;
         }
